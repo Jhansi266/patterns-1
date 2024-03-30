a4826fdf6adcf12865c8614f67620ac4fcaeba00
Jhansi266 <155867745+Jhansi266@users.noreply.github.com>
2024-03-30 18:27:01 +0530

    Add files via upload

---
+++ python patterns.py
@@ -0,0 +1,43 @@
+#print 3 star pattern
+n=3
+for i in range(1,4):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+
+
+print()
+
+#print 5 star pattern
+n=5
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+
+print()
+
+#print square pattern
+n=5
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=" ")
+    print()
+
+print()
+#print 5 star pattern decrement
+n=5
+for i in range(1,6):
+    for j in range(1,7-i):
+        print("*",end=" ")
+    print()
+
+
+print()
+n=5
+for i in range(1,6):
+    for j in range(i,6):
+        print("*",end=" ")
+    print()
